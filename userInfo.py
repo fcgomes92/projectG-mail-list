77930df90d7964a11bab4a3acd4fc2988d790b05
gomes <fcgomes.92@gmail.com>
2015-02-24 01:22:02 -0300

    Ajuste de split.

--- userInfo.py
+++ userInfo.py
@@ -2,8 +2,8 @@ page_request = __import__('getPageHtml')
 server_request = __import__('serverSTMP')
 
 url = 'http://sites.google.com/site/frinhanimd/disciplinas/sit240'
-split1 = ''
-split2 = ''
+split1 = 'AVISOS</span></b></font> </div><div>'
+split2 = '</div></td>'
 
 msg = page_request.getPage(url, split1, split2)
 
